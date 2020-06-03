57f259780b3d8431c1971935862bdcb87fc73f3c
xiexingen <1002275364@qq.com>
2020-06-03 16:48:58 +0800

    路由前缀

--- src/route-helper.ts
+++ src/route-helper.ts
@@ -5,7 +5,7 @@
  */
 export function filterRoutesByGroups(arr: any[], groups: string[] = []) {
   const result: any[] = [];
-  arr.forEach((item) => {
+  arr.forEach(item => {
     if (!item.group || groups.indexOf(item.group) !== -1) {
       const newItem = item;
       if (item.routes && item.routes.length > 0) {
@@ -28,7 +28,7 @@ export function filterRoutesByGroups(arr: any[], groups: string[] = []) {
  */
 export function findRouteMenuByGroup(arr: any[], group: string): any {
   let result: any;
-  arr.forEach((item) => {
+  arr.forEach(item => {
     if (item.group && item.group === group) {
       result = item;
       return;
@@ -61,7 +61,7 @@ export function configRoutePrefix(prefix: string) {
  * @param prefix 要设置的前缀
  */
 export function patchRouteBase(arr: any[]): void {
-  arr.forEach((item) => {
+  arr.forEach(item => {
     // eslint-disable-next-line no-param-reassign
     item.path = `${routePrefix}${item.path}`;
     if (item.routes && item.routes.length > 0) {
